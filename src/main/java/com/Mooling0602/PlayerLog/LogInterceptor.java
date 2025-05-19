7dc9528413c52e20c61353042d70bc34667fb358
Mooling0602 <clemooling@outlook.com>
2025-05-19 11:35:22 +0800

    feat: add dynamic command support, developing

--- src/main/java/com/Mooling0602/PlayerLog/LogInterceptor.java
+++ src/main/java/com/Mooling0602/PlayerLog/LogInterceptor.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.RemoteConsoleCommandSender;
+import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -15,16 +16,55 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class LogInterceptor extends JavaPlugin implements Listener {
 
     private boolean chatInterceptEnabled = false; // 默认不拦截聊天消息
+    private String dynamicString = "debug";
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
         getCommand("chatmsg").setExecutor(this);
+        registerDynamicCommand();
         getLogger().info(ChatColor.GREEN + "PlayerLog 已启用！" + ChatColor.RESET);
+    }
+
+    private void registerDynamicCommand() {
+        Command dynamicCommand = new Command(dynamicString) {
+            @Override
+            public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+                sender.sendMessage("执行了动态注册命令！");
+                return true;
+            }
+        };
+
+        try {
+            SimpleCommandMap commandMap = (SimpleCommandMap) Bukkit.getServer()
+                    .getClass().getMethod("getCommandMap").invoke(Bukkit.getServer());
+            commandMap.register("dynamiccmd", dynamicCommand);
+            getLogger().info("动态命令注册成功！");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void unregisterDynamicCommand() {
+        try {
+            SimpleCommandMap commandMap = (SimpleCommandMap) Bukkit.getServer()
+                    .getClass().getMethod("getCommandMap").invoke(Bukkit.getServer());
+            java.lang.reflect.Field knownCommandsField = SimpleCommandMap.class.getDeclaredField("knownCommands");
+            knownCommandsField.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            Map<String, Command> knownCommands = (Map<String, Command>) knownCommandsField.get(commandMap);
+            if (knownCommands.containsKey(dynamicString)) {
+                knownCommands.remove(dynamicString);
+                getLogger().info("动态命令已取消注册！");
+            } else {getLogger().info("没有要取消注册的动态命令！");}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -51,7 +91,7 @@ public class LogInterceptor extends JavaPlugin implements Listener {
         if (chatInterceptEnabled) {
             event.setCancelled(true);
         }
-        getLogger().info("<" + event.getPlayer().getName() + "> " + event.getMessage());
+        Bukkit.getConsoleSender().sendMessage("<" + event.getPlayer().getName() + "> " + event.getMessage());
     }
 
     @Override
@@ -61,10 +101,10 @@ public class LogInterceptor extends JavaPlugin implements Listener {
                 sender.sendMessage(ChatColor.RED + "该命令只能通过控制台或Rcon执行！" + ChatColor.RESET);
                 return true;
             }
-            if (args.length != 1) {
-                getLogger().info(ChatColor.YELLOW + "用法: 在控制台执行chatmsg on|off" + ChatColor.RESET);
-                return true;
-            }
+            // if (args.length != 1) {
+            //     getLogger().info(ChatColor.YELLOW + "用法: 在控制台执行chatmsg on|off" + ChatColor.RESET);
+            //     return true;
+            // }
             if (args[0].equalsIgnoreCase("on")) {
                 chatInterceptEnabled = false;
                 sender.sendMessage(ChatColor.GOLD + "客户端聊天拦截功能已关闭" + ChatColor.GRAY + "，若MCDR插件功能未关闭会出现聊天消息重复或功能无法使用等异常情况，" + ChatColor.RESET + ChatColor.GOLD + "BukkitChatManager需配置启用兼容模式（compatibility_mode）" + ChatColor.RESET);
@@ -73,21 +113,49 @@ public class LogInterceptor extends JavaPlugin implements Listener {
                 chatInterceptEnabled = true;
                 sender.sendMessage(ChatColor.GREEN + "客户端聊天拦截功能已开启" + ChatColor.GRAY + "，MCDR插件可开始接管聊天内容，" + ChatColor.RESET + ChatColor.GOLD + "BukkitChatManager需配置禁用兼容模式（compatibility_mode）" + ChatColor.RESET);
             }
-            else {
-                getLogger().info(ChatColor.YELLOW + "正确用法: 在控制台执行chatmsg on|off" + ChatColor.RESET);
+            else if (args[0].equalsIgnoreCase("set_test_node")) {
+                if (args.length != 2) {
+                    sender.sendMessage(ChatColor.GRAY + "用法: chatmsg set_test_node <新值>" + ChatColor.RESET);
+                    return true;
+                }
+                unregisterDynamicCommand();
+                dynamicString = args[1];
+                registerDynamicCommand();
+                sender.sendMessage("[调试] dynamicString 已更新为: " + ChatColor.YELLOW + dynamicString + ChatColor.RESET);
+                return true;
             }
+            else
+                getLogger().info(ChatColor.YELLOW + "正确用法: 在控制台执行chatmsg on|off" + ChatColor.RESET);
+            sender.sendMessage("[调试] 当前命令节点数量有：" + args.length);
             return true;
         }
         return false;
     }
 
-
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         List<String> completions = new ArrayList<>();
-        if (command.getName().equalsIgnoreCase("/chatmsg")) {
-            completions.add("on");
-            completions.add("off");
+        if (command.getName().equalsIgnoreCase("chatmsg")) {
+            if (args.length == 1) {
+                completions.add("on");
+                completions.add("off");
+            }
+            if (args.length == 2) {
+                if (args[0].equalsIgnoreCase(dynamicString)) {
+                    completions.add("test");
+                }
+            }
+            if (args.length > 2) {
+                completions = new ArrayList<>();
+            }
+        }
+        if (command.getName().equalsIgnoreCase("!!MCDR")) {
+            if (args.length == 1) {
+                completions.add("status");
+            }
+        }
+        if (args.length == 0) {
+            completions.add("!!MCDR");
         }
         return completions;
     }
